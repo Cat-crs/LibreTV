b56d467d64837a92de87b1a08d5e3fa901599a0e
Cat-crs <3055431604@qq.com>
2025-11-29 16:41:51 +0800

    Add new customer sites to CUSTOMER_SITES

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,26 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
+    },
+    zy360: {
+        api: 'https://360zyzz.com/api.php/provide/vod',
+        name: '360资源',
+    },
+    hongniu: {
+        api: 'https://www.hongniuzy2.com/api.php/provide/vod/at/josn',
+        name: '红牛资源',
+    },
+    liangzi: {
+        api: 'https://cj.lziapi.com/api.php/provide/vod/at/json',
+        name: '量子资源',
+    },
+    feifan: {
+        api: 'http://api.ffzyapi.com/api.php/provide/vod/at/json',
+        name: '非凡资源',
+    },
+    suoni: {
+        api: 'https://suoniapi.com/api.php/provide/vod/?ac=list',
+        name: '索尼资源',
     }
 };
 
